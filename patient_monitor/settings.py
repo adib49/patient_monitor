7bb14df4999b3709ef7346d07368b52c17f6f9c9
Adib M <adibw449@gmail.com>
2025-02-12 23:11:36 +0530

    Ready for Testing

--- patient_monitor/settings.py
+++ patient_monitor/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = 'django-insecure-dummy-secret-key-for-easy-assessment' #easy secret
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+
 
 
 # Application definition
